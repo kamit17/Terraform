4dbd77a29917c7061a00c8987820295069b722f7
Amit Kumar <kamit17@outlook.com>
2025-10-03 18:31:55 -0500

    updated directory structure

---
+++ kplabs-terraform/first-lab/first_ec2.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "us-east-2"
+  #access_key = "my-access-key"
+  #secret_key = "my-secret-key"
+}
+variable "dev_names" {
+  type = list
+  default =["greg","shomari","CDN"]  
+}
+resource "aws_instance" "myec2" {
+  ami = "ami-0cfde0ea8edd312d4"
+  instance_type = "t2.micro"
+  count = 2
+
+  tags = {
+    Name = "test-${count.index}"
+  }
+  
+}
+
+resource "aws_iam_user" "this" {
+  name = "payments-user-${count.index}"
+  count = 2
+  
+}
+
+resource "aws_iam_user" "dev" {
+  name = var.dev_names[count.index]
+  count = 3
+  
+}
---
+++ kplabs-terraform/first-lab/readme.md
@@ -0,0 +1,8 @@
+```
+export the token via the environment variable
+export TF_VAR_token="ghp_XXXXXXXXXXXXXXXXXX"
+
+export TF_VAR_aws_access_key="your-access-key"
+export TF_VAR_aws_secret_key="your-secret-key"
+
+```
