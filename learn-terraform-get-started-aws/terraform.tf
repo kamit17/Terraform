7946d27d9731b2cb69aba6a08908c33c2cde0951
kamit17 <kamit17@outlook.com>
2025-10-26 09:51:34 -0500

    Added HCP config

--- learn-terraform-get-started-aws/terraform.tf
+++ learn-terraform-get-started-aws/terraform.tf
@@ -1,4 +1,12 @@
 terraform {
+  cloud {
+    organization = "thinklab"
+
+    workspaces {
+      project = "Learn Terraform"
+      name    = "learn-terraform-aws-get-started"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
